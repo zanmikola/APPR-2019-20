5dc9189a9615cb091d032336bf780a1eb0197ac1
zanmikola <zan.mikola@student.fmf.uni-lj.si>
2020-01-07 22:08:26 +0100

    Popravek map in knjižnic

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,8 +1,9 @@
 library(knitr)
 library(rvest)
 library(gsubfn)
-library(tidyr)
 library(shiny)
-
+library(readr)
+library(tidyr)
+library(dplyr)
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
